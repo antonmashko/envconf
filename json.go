e25e26bfb6b2c1ef6efda399829333ae82e038df
begmaroman <begmaroman@gmail.com>
2018-07-30 16:58:20 +0300

    fix json contains key functions

--- json.go
+++ json.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"io/ioutil"
+	"reflect"
 	"strings"
 )
 
@@ -11,8 +12,10 @@ const JsonConfigName = "json"
 
 type JsonConfig struct {
 	filepath string
-	values   map[string]interface{}
+	values   JsonConfigValues
 }
+
+type JsonConfigValues map[string]interface{}
 
 func (j *JsonConfig) SetFilePathFlag(flagName string, defaultPath string) {
 	flag.StringVar(&j.filepath, flagName, defaultPath, "json config file path")
@@ -35,14 +38,14 @@ func (j *JsonConfig) Contains(keyName string) bool {
 	values := strings.Split(keyName, string(Separator))
 	m := j.values
 	for i, v := range values {
-		tm, ok := m[strings.ToLower(v)]
+		tm, ok := m[v]
 		if !ok {
 			return false
 		}
 		if i+1 == len(values) {
 			break
 		}
-		m, ok = tm.(map[string]interface{})
+		m, ok = tm.(JsonConfigValues)
 		if !ok {
 			return false
 		}
@@ -58,5 +61,47 @@ func (j *JsonConfig) Unmarshal(data interface{}) error {
 	if err := json.Unmarshal(b, data); err != nil {
 		return err
 	}
-	return json.Unmarshal(b, &j.values)
+
+	j.values = j.createJsonConfigValues(reflect.ValueOf(data))
+
+	return nil
+}
+
+func (j *JsonConfig) createJsonConfigValues(val reflect.Value) (values JsonConfigValues) {
+	if val.Kind() == reflect.Interface && !val.IsNil() {
+		elm := val.Elem()
+		if elm.Kind() == reflect.Ptr && !elm.IsNil() && elm.Elem().Kind() == reflect.Ptr {
+			val = elm
+		}
+	}
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	values = make(JsonConfigValues)
+
+	for i := 0; i < val.NumField(); i++ {
+		valueField := val.Field(i)
+		typeField := val.Type().Field(i)
+
+		if valueField.Kind() == reflect.Interface && !valueField.IsNil() {
+			elm := valueField.Elem()
+			if elm.Kind() == reflect.Ptr && !elm.IsNil() && elm.Elem().Kind() == reflect.Ptr {
+				valueField = elm
+			}
+		}
+
+		if valueField.Kind() == reflect.Ptr {
+			valueField = valueField.Elem()
+
+		}
+
+		if valueField.Kind() == reflect.Struct {
+			values[typeField.Name] = j.createJsonConfigValues(valueField)
+		} else {
+			values[typeField.Name] = valueField
+		}
+	}
+
+	return
 }
